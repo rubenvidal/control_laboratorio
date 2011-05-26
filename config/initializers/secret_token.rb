6a4bfc750f227b2d28b8c7f000ef5e3bbcf03854
Rubén Vidal <ruben.vidal.castellet@gmail.com>
2011-05-26 12:07:50 +0200

    aplicacion inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ControlLaboratorio::Application.config.secret_token = 'dca94b65d10c398d7c3454e2f5cefa7e458a24fb856c2d89b9f537d7105387afe36d5fb935e1c966be3be5819b2b5357596db84153a29f3641e8369a0db2d876'
